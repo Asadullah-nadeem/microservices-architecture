de41b79053f00a06fa0b1859bec6a3892c9b75a4
Muhammad <asadullahnadeem48@gmail.com>
2025-08-16 17:49:53 +0530

    update

--- car-webservices/src/main/java/com/example/car_webservices/config/AppConfig.java
+++ car-webservices/src/main/java/com/example/car_webservices/config/AppConfig.java
@@ -6,6 +6,10 @@
 //import springfox.documentation.spi.DocumentationType;
 //import springfox.documentation.spring.web.plugins.Docket;
 //import springfox.documentation.swagger2.annotations.EnableSwagger2;
+/**
+ * @implNote Delete the AppConfig.java file. It is completely unnecessary and is the source of the compile errors.
+ */
+
 
 //
 //@Configuration
