a07703373e76f0ddd2191e018364fffc468690b1
Muhammad <asadullahnadeem48@gmail.com>
2025-08-16 14:02:41 +0530

    update

--- car-webservices/src/test/java/com/example/car/challenge/controller/VehicleControllerTest.java
+++ car-webservices/src/test/java/com/example/car/challenge/controller/VehicleControllerTest.java
@@ -1,4 +1,30 @@
 package com.example.car.challenge.controller;
 
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(VehicleControllerTest.class)
+@AutoConfigureMockMvc
 public class VehicleControllerTest  {
+
+
+
+ /**
+ * @deprecated MockMvc is a component of the Spring Test framework used for testing Spring MVC applications.
+  */
+ @Autowired
+ MockMvc mockMvc;
+
+// @MockBean
+// VehicleControllerTest vehicleControllerTest;
+
+
+
+
 }
