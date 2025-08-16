4c986327f88c0bb893cc6835078c7e4ed50fadbc
Muhammad <asadullahnadeem48@gmail.com>
2025-08-16 14:46:04 +0530

    update

---
+++ car-webservices/src/main/java/com/example/car_webservices/model/polygon/Computed.java
@@ -0,0 +1,12 @@
+package com.example.car_webservices.model.polygon;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Computed {
+    private ActiveTimedOptions activeTimedOptions;
+}
---
+++ car-webservices/src/main/java/com/example/car_webservices/model/polygon/TimedOption.java
@@ -0,0 +1,15 @@
+package com.example.car_webservices.model.polygon;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TimedOption {
+    private String key;
+    private List<List<Double>> changesOverTime = null;
+}
