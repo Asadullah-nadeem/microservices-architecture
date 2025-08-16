3bb1a595d102a039307803e1a6ff20c63eb24f23
Muhammad <asadullahnadeem48@gmail.com>
2025-08-16 15:31:53 +0530

    update

--- car-position-producer/src/main/java/com/example/car_position_producer/producer/VehicleEventProducer.java
+++ car-position-producer/src/main/java/com/example/car_position_producer/producer/VehicleEventProducer.java
@@ -9,6 +9,7 @@ import org.apache.kafka.common.header.internals.RecordHeader;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+import java.util.concurrent.CompletableFuture;
 
 import java.util.List;
 
@@ -26,16 +27,32 @@ public class VehicleEventProducer {
         this.objectMapper = objectMapper;
     }
 
-    public ListenableFuture<SendResult<String,Vehicle>> sendVehicleEvents(Vehicle vehicle) {
+//    public ListenableFuture<SendResult<String,Vehicle>> sendVehicleEvents(Vehicle vehicle) {
+//
+//        String key = vehicle.getVin();
+//
+//        ProducerRecord<String,Vehicle> producerRecord = buildProducerRecord(key, vehicle, topic);
+//
+//        ListenableFuture<SendResult<String,Vehicle>> listenableFuture =  kafkaTemplate.send(producerRecord);
+//        listenableFuture.addCallback(new VehicleEventListenableFutureCallback(key,vehicle));
+//
+//        return listenableFuture;
+//    }
 
+    public CompletableFuture<SendResult<String, Vehicle>> sendVehicleEvents(Vehicle vehicle) {
         String key = vehicle.getVin();
+        ProducerRecord<String, Vehicle> producerRecord = buildProducerRecord(key, vehicle, topic);
 
-        ProducerRecord<String,Vehicle> producerRecord = buildProducerRecord(key, vehicle, topic);
+        CompletableFuture<SendResult<String, Vehicle>> future = kafkaTemplate.send(producerRecord);
+        future.whenComplete((sendResult, throwable) -> {
+            if (throwable != null) {
+                log.error("Failed to send event for {}: {}", key, throwable.getMessage(), throwable);
+            } else {
+                log.info("Successfully sent event for {}: {}", key, vehicle);
+            }
+        });
 
-        ListenableFuture<SendResult<String,Vehicle>> listenableFuture =  kafkaTemplate.send(producerRecord);
-        listenableFuture.addCallback(new VehicleEventListenableFutureCallback(key,vehicle));
-
-        return listenableFuture;
+        return future;
     }
 
     private ProducerRecord<String, Vehicle> buildProducerRecord(String key, Vehicle value, String topic) {
